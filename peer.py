c29c76eaa0ed569f439de5945d83491d8b88b1d7
nburnet1 <nburnet1@unca.edu>
2023-03-17 18:41:46 -0400

    removed css from readme

--- peer.py
+++ peer.py
@@ -1,1 +1,32 @@
+import pickle
+import socket
+import threading
+"""
+Peer class that has all functionalities for p2p messaging.
 
+A peer can:
+Connect
+Authenticate
+Broadcast
+Send
+Receive
+Leave
+"""
+
+
+class Peer:
+    def __init__(self):
+        print("Creating Peer")
+
+    def connect(self):
+        return
+    def authenticate(self):
+        return
+    def broadcast(self):
+        return
+
+    def send(self):
+        return
+
+    def leave(self):
+        return
