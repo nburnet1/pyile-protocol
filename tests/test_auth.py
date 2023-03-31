b8c79280cb36a1b487d2bd420c4e68b0d58ddc48
nburnet1 <nburnet1@unca.edu>
2023-03-31 01:37:12 +0000

    test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,8 +4,8 @@ from lib.peers.AuthPeer import AuthPeer
 
 
 def test_auth():
-    # auth_peer = AuthPeer(("192.168.1.66", 4702), 1, "password")
-    auth_peer = AuthPeer(("172.20.100.39", 4702), 1, "password")
+    auth_peer = AuthPeer(("192.168.1.66", 4702), 1, "password")
+    # auth_peer = AuthPeer(("172.20.100.39", 4702), 1, "password")
     print(auth_peer)
     try:
         auth_thread = threading.Thread(target=auth_peer.authenticate_peers)
--- tests/test_peer.py
+++ tests/test_peer.py
@@ -4,11 +4,11 @@ from random import randint
 
 def test_peer():
     # peer
-    # peer = JoinPeer(("192.168.1.65", 4702+randint(0, 20)))
-    peer = JoinPeer(("172.20.100.99", 4702 + randint(0, 20)))
+    peer = JoinPeer(("192.168.1.65", 4702+randint(0, 20)))
+    # peer = JoinPeer(("172.20.100.99", 4702 + randint(0, 20)))
     print(peer)
     # initial peer
-    authenticated = peer.get_authenticated(("172.20.100.39", 4702), "password")
-    # authenticated = peer.get_authenticated(("192.168.1.66", 4702), "password")
+    # authenticated = peer.get_authenticated(("172.20.100.39", 4702), "password")
+    authenticated = peer.get_authenticated(("192.168.1.66", 4702), "password")
     print(peer)
     peer.connect()
