912c43dc3a27510b870df1a72fd17434e6c698c9
nburnet1 <nburnet1@unca.edu>
2023-10-25 16:40:21 +0000

    incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyile_protocol',
-    version='0.1.0',
+    version='0.1.1',
     description='Protocol library for p2p messaging and authentication.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
